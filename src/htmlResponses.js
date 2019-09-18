e9826ff1b08ef108fddffe91444530b77eb165c3
Ian Matic (RIT Student) <imm2853@ad.rit.edu>
2019-09-18 14:46:44 -0400

    working on assignment

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 
 let index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const css = fs.readFileSync(`${__dirname}/../client/style.css`);
 
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(index);
+  response.end();
+};
+
+const getCSS = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/css' });
+  response.write(css);
   response.end();
 };
 
@@ -13,3 +20,4 @@ const setIndex = (value) => {
 };
 module.exports.getIndex = getIndex;
 module.exports.setIndex = setIndex;
+module.exports.getCSS = getCSS;
--- src/server.js
+++ src/server.js
@@ -1,41 +1,61 @@
 const http = require('http');
 const fs = require('fs');
+const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
-const mediaHandler = require('./mediaResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+const url = require('url');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    const res = response;
+
+    const body = [];
+
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
+
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+
+      jsonHandler.addUser(request, res, bodyParams);
+    });
+  }
+};
+
+const handleGet = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response);
+  } else if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsers(request, response);
+  } else {
+    htmlHandler.getIndex(request, response);
+  }
+};
 
 const onRequest = (request, response) => {
   console.log(request.url);
 
-  switch (request.url) {
-    case '/':
-      htmlHandler.setIndex(fs.readFileSync(`${__dirname}/../client/client.html`));
-      htmlHandler.getIndex(request, response);
-      break;
-    case '/page2':
-      htmlHandler.setIndex(fs.readFileSync(`${__dirname}/../client/client2.html`));
-      htmlHandler.getIndex(request, response);
-      break;
-    case '/page3':
-      htmlHandler.setIndex(fs.readFileSync(`${__dirname}/../client/client3.html`));
-      htmlHandler.getIndex(request, response);
-      break;
-    case '/party.mp4':
-      mediaHandler.getParty(request, response);
-      break;
-    case '/bling.mp3':
-      mediaHandler.getBling(request, response);
-      break;
-    case '/bird.mp4':
-      mediaHandler.getBird(request, response);
-      break;
-    default:
-      htmlHandler.getIndex(request, response);
-      break;
+  const parsedUrl = url.parse(request.url);
+
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGet(request, response, parsedUrl);
   }
 };
 
 http.createServer(onRequest).listen(port);
 
 console.log(`Listening on 127.0.0.1: ${port}`);
+
+
